247226995be7769fab2b4f384e862207cf7510ad
lwto <linwathanoo227@gmail.com>
2021-09-20 20:22:27 +0630

    twilio

---
+++ Twilio App/credentials.py
@@ -0,0 +1,4 @@
+account_sid = 'ACf18dc13957e4a93b7caadcaf13068934'
+auth_token = '0c800fbec9507428b227613c53bc9764'
+my_cell = 'whatsapp:+959976323314'
+my_twilio = 'whatsapp:+14155238886'
---
+++ Twilio App/readme.md
@@ -0,0 +1,7 @@
+# Python Text App Using Twilio API
+------
+
+***This app is created using***
+
+#### 1.Twilio API 
+#### 2.Python
